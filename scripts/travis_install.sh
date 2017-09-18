446dca581f64fe8c0e935fdb8bb1aa542c82f7fe
Dave Tillman <dtillman@pivotal.io>
2017-09-18 12:37:10 -0600

    Add vnext configuration

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -3,10 +3,12 @@
 dotnet --info
 
 export CI_BUILD=True
-export STEELTOE_VERSION=1.1.1
+export STEELTOE_VERSION=2.0.0
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then cp config/nuget-master.config ./nuget.config ; fi
 if [[ "$TRAVIS_BRANCH" == "dev" ]]; then cp config/nuget-dev.config ./nuget.config ; fi
+if [[ "$TRAVIS_BRANCH" == "vnext" ]]; then cp config/nuget-vnext.config ./nuget.config ; fi
 if [[ "$TRAVIS_TAG" != "" ]]; then cp config/nuget.config ./nuget.config ; fi
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then cp config/versions.props ./versions.props ; fi
 if [[ "$TRAVIS_BRANCH" == "dev" ]]; then cp config/versions.props ./versions.props ; fi
+if [[ "$TRAVIS_BRANCH" == "vnext" ]]; then cp config/versions.props ./versions.props ; fi
 if [[ "$TRAVIS_TAG" != "" ]]; then cp config/versions.props ./versions.props ; fi
