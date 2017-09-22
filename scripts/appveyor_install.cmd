19770fbc0f1820c503ab67efeb16a40506bedf5d
Dave Tillman <dtillman@pivotal.io>
2017-09-22 07:30:08 -0600

    Test update builds

--- scripts/appveyor_install.cmd
+++ scripts/appveyor_install.cmd
@@ -15,13 +15,12 @@ IF NOT "%STEELTOE_VERSION_SUFFIX%"=="" (
 )   
 echo "Version Suffix:" %STEELTOE_VERSION_SUFFIX%
 SET BUILD_TYPE=Release
+COPY config\nuget.config .\nuget.config
+COPY config\versions.props .\versions.props
 IF "%APPVEYOR_REPO_BRANCH%"=="master" COPY config\nuget-master.config .\nuget.config
 IF "%APPVEYOR_REPO_BRANCH%"=="dev" COPY config\nuget-dev.config .\nuget.config
-IF "%APPVEYOR_REPO_BRANCH%"=="vnext" COPY config\nuget-vnext.config .\nuget.config
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" COPY config\nuget.config .\nuget.config
 IF "%APPVEYOR_REPO_BRANCH%"=="master" COPY config\versions.props .\versions.props
 IF "%APPVEYOR_REPO_BRANCH%"=="dev" COPY config\versions.props .\versions.props
-IF "%APPVEYOR_REPO_BRANCH%"=="vnext" COPY config\versions.props .\versions.props
 IF NOT "%APPVEYOR_REPO_TAG_NAME%"=="" COPY config\versions.props .\versions.props
 IF "%APPVEYOR_REPO_BRANCH%"=="dev" SET BUILD_TYPE=Debug
-IF "%APPVEYOR_REPO_BRANCH%"=="vnext" SET BUILD_TYPE=Debug
--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -4,11 +4,11 @@ dotnet --info
 
 export CI_BUILD=True
 export STEELTOE_VERSION=2.0.0
+cp config/nuget.config ./nuget.config
+cp config/versions.props ./versions.props
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then cp config/nuget-master.config ./nuget.config ; fi
 if [[ "$TRAVIS_BRANCH" == "dev" ]]; then cp config/nuget-dev.config ./nuget.config ; fi
-if [[ "$TRAVIS_BRANCH" == "vnext" ]]; then cp config/nuget-vnext.config ./nuget.config ; fi
 if [[ "$TRAVIS_TAG" != "" ]]; then cp config/nuget.config ./nuget.config ; fi
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then cp config/versions.props ./versions.props ; fi
 if [[ "$TRAVIS_BRANCH" == "dev" ]]; then cp config/versions.props ./versions.props ; fi
-if [[ "$TRAVIS_BRANCH" == "vnext" ]]; then cp config/versions.props ./versions.props ; fi
 if [[ "$TRAVIS_TAG" != "" ]]; then cp config/versions.props ./versions.props ; fi
